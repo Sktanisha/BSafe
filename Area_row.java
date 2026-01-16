46fb83b8e68dd7ca448ca81c2323eacc2dac2527
Sk. Sanjida Tanisha <94749695+Sktanisha@users.noreply.github.com>
2026-01-16 17:56:20 -0500

    Add files via upload

---
+++ Area_row.java
@@ -0,0 +1,14 @@
+package edu.bd.ewu.besafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Area_row extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_area_row);
+    }
+}
