a0dcaa527b60ec3eb630e4632e0714267e6b702d
Sk. Sanjida Tanisha <94749695+Sktanisha@users.noreply.github.com>
2026-01-16 17:57:06 -0500

    Add files via upload

---
+++ Row_All_Area.java
@@ -0,0 +1,14 @@
+package edu.bd.ewu.besafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Row_All_Area extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_row_all_area);
+    }
+}
